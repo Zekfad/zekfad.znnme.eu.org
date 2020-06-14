406650a96f7694283aa560879dcb0984732e900c
Yaroslav Vorobev <vorobevyaroslav@gmail.com>
2020-06-14 21:57:52 +0300

    feat: error page

--- client/lang/en.js
+++ client/lang/en.js
@@ -3,6 +3,23 @@ export default {
 		titleTemplate: '%s - Yaroslav Vorobev',
 	},
 	pages: {
+		error: {
+			meta: {
+				title      : 'Error',
+				description: 'Error page.',
+			},
+			errors: {
+				404: {
+					title      : 'Page not found',
+					description: 'Page you requested doesn\'t exist.',
+				},
+				unknown: {
+					title      : 'An error occurred.',
+					description: 'Looks like this is the problem on our side, you can report us about this.',
+				},
+			},
+			goBack: 'Go back',
+		},
 		home: {
 			meta: {
 				title      : 'Homepage',
---
+++ client/layouts/error.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="error">
+		<h1 class="error__title">
+			{{ 404 === error.statusCode
+				? $t('pages.error.errors.404.title')
+				: $t('pages.error.errors.unknown.title') }}
+		</h1>
+		<p class="error__description">
+			{{ 404 === error.statusCode
+				? $t('pages.error.errors.404.description')
+				: $t('pages.error.errors.unknown.description') }}
+		</p>
+		<I18nLink :to="pathBack" class="error__go-back-button">
+			{{ $t('pages.error.goBack') }}
+		</I18nLink>
+	</div>
+</template>
+
+<script>
+import I18nLink from '~/components/I18nLink';
+
+export default {
+	components: {
+		I18nLink,
+	},
+	props: {
+		error: {
+			type   : null,
+			default: {},
+		},
+	},
+	computed: {
+		pathBack() {
+			let { $routerHistory, } = this,
+				path;
+
+			if (!$routerHistory || !$routerHistory.hasPrevious())
+				path = '/';
+
+			return path || $routerHistory.previous().path;
+		},
+	},
+	head() {
+		return {
+			title: 404 === this.error.statusCode
+				? this.$t('pages.error.errors.404.title')
+				: this.$t('pages.error.errors.unknown.title'),
+		};
+	},
+};
+</script>
