712f1ccf6ba184b62cfb843a08e54b778db81f13
Yaroslav Vorobev <vorobevyaroslav@gmail.com>
2020-06-14 21:57:03 +0300

    feat: i18n link component

---
+++ client/components/I18nLink.vue
@@ -0,0 +1,25 @@
+<template>
+	<nuxt-link :to="localePath(to)" v-bind="attrs">
+		<slot></slot>
+	</nuxt-link>
+</template>
+
+<script>
+export default {
+	props: {
+		to: {
+			type   : String,
+			default: '/',
+		},
+	},
+	data() {
+		let attrs = this.$attrs;
+
+		delete attrs.to;
+
+		return {
+			attrs,
+		};
+	},
+};
+</script>
